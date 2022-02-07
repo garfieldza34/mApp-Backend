15fb2bbe9fd75e91e1d1aba6d2bb0f09ef7a7fe4
garfieldza34 <75066154+garfieldza34@users.noreply.github.com>
2022-02-07 13:26:38 +0700

    week 11

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const cors = require("cors")
+const cors = require("cors");
 
 const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-const usersRouter_mongo = require("./routes/mongo/users.js")
-const productsRouter_mongo = require("./routes/mongo/products.js")
+const usersRouter_mongo = require("./routes/mongo/users");
+const productsRouter_mongo = require("./routes/mongo/products");
+const customersRouter_mongo = require("./routes/mongo/customers");
 
 var app = express();
 
-app.use(cors())
+app.use(cors());
 
-mongoose.connect("mongodb+srv://sigren:gf0877374410@mapp.dz5xb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://mapp:mapp1234@mapp.af4hu.mongodb.net/mApp?retryWrites=true&w=majority");
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use("/mongo", usersRouter_mongo)
-app.use("/mongo", productsRouter_mongo)
+app.use("/mongo", usersRouter_mongo);
+app.use("/mongo", productsRouter_mongo);
+app.use("/mongo", customersRouter_mongo);
 
 module.exports = app;
