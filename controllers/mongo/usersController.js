bac994b50b2daae8dcc3feb47bdf718277fa8f9e
garfieldza34 <75066154+garfieldza34@users.noreply.github.com>
2022-02-09 16:56:06 +0700

    week 12

--- controllers/mongo/usersController.js
+++ controllers/mongo/usersController.js
@@ -1,13 +1,14 @@
-const users = require("../../models/mongo/users.js")
+const users = require("../../models/mongo/users.js");
 
 exports.insert = async (req, res, next) => {
     let data = new users({
-        email:"sigren8104@gmail.com",
-        name: "Sigren",
-        lastname:"Samuii"
-    })
-    data.save()
+        email: "garfieldza34@gmail.com",
+        name: "จักรกฤษ",
+        lastname: "สุขาจารย์"
+    });
+
+    data.save();
     res.status(200).json({
-        message:"บันทึกข้อมูลเรียบร้อย"
-    })
+        message: "บันทึกข้อมูลเรียบร้อยแล้ว"
+    });
 }
